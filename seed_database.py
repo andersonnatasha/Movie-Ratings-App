aea1339e9efae69c373fa1d71548dca99c476b53
Natasha Anderson <andersonnatashaa@gmail.com>
2020-10-30 19:25:21 +0000

    connect to db, create tables, load data

--- seed_database.py
+++ seed_database.py
@@ -10,3 +10,10 @@ import model
 import server
 
 os.system('dropdb ratings')
+os.system('createdb ratings')
+
+model.connect_to_db(server.app)
+model.db.create_all()
+
+with open('data/movies.json') as f:
+    movie_data = json.load(f.read())
