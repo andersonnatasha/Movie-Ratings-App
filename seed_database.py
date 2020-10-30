cfc64593795551252b847eb4d182aabe3a1afe94
Natasha Anderson <andersonnatashaa@gmail.com>
2020-10-30 20:04:18 +0000

    seeding movie db

--- seed_database.py
+++ seed_database.py
@@ -15,5 +15,22 @@ os.system('createdb ratings')
 model.connect_to_db(server.app)
 model.db.create_all()
 
+#Load movies from JSON file
 with open('data/movies.json') as f:
     movie_data = json.load(f.read())
+
+# Create movies, store them in list so
+# we can use them to create fake ratings later
+movies_in_db = []
+for movie in movie_data:
+    title, overview, poster_path = (movie['title'],
+                                    movie['overview'],
+                                    movie['poster_path'])
+    release_date = datetime.strptime(movie['release_date'], '%Y-%m-%d')
+
+    db_movie = crud.create_movie(title,
+                                overview,
+                                release_date,
+                                poster_path)
+    movies_in_db.append(db_movie)
+
